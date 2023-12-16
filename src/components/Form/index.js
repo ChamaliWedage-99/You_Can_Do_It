04d809a02cea61233ea1e21f592951cc2d005015
Chamali Wedage <94621140+ChamaliWedage-99@users.noreply.github.com>
2023-12-16 17:01:51 +0530

    updated

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { FormContainer, Input, Button } from './styles';
 
-function Form() {
+function Form({input, setInput}) {
   return (
-    <FormContainer><Input/>
-    <Button>Add</Button>
+    <FormContainer>
+        <Input value={input} onChange={(e) => setInput(e.target.value)} type='text' role='input'/>
+    <Button type=''>Add</Button>
     </FormContainer>
   )
 }
--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import Form from '../Form'
-import { Container } from './styles'
+import React, {useState} from 'react';
+import Form from '../Form';
+import { Container } from './styles';
 
 function Todo() {
+    const {input, setInput} = useState('');
+    console.log(input, "input");
+
   return (
     <Container><h2>To Do List</h2>
     {/*Form Component */}
-    <Form/>
+    <Form input={input} setInput = {setInput}/>
      {/*ToDo List */}
       {/* Key*/}
        {/* Author Component*/}
