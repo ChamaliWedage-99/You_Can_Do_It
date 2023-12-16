07b7b5f9c1683cb74adb3000c8ca27844e6f3ec9
Chamali Wedage <94621140+ChamaliWedage-99@users.noreply.github.com>
2023-12-16 16:39:50 +0530

    updated

---
+++ src/components/Form/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.form``;
+
+export const Input = styled.input``;
+
+export const Button = styled.button``;
--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Form from '../Form'
 import { Container } from './styles'
 
 function Todo() {
   return (
     <Container><h2>To Do List</h2>
     {/*Form Component */}
+    <Form/>
      {/*ToDo List */}
       {/* Key*/}
        {/* Author Component*/}
